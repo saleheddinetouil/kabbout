7d2a1fc2d11fe2b97e3e21709561cd67c7400799
Saleh eddine Touil <168127706+saleheddinetouil@users.noreply.github.com>
2024-07-21 19:11:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -182,7 +182,7 @@ if game.round_history:
         if sum(round_scores.values()) == 0:
             st.subheader(f"Round {i+1} (Frish Round)")
         # elif one score added then its a frish rouncd
-        elif sum(round_scores.values()) == 100 or sum(round_scores.values()) == 150:
+        elif len(round_scores) == 1 and sum(round_scores.values()) == 100 or sum(round_scores.values()) == 150:
             st.subheader(f"Round {i+1} Ghlot feha {list(round_scores.keys())[0]}")
         else:
             st.subheader(f"Round {i+1}")
